6dd095450714dcf37cf63a49bcdc082ce869a5d9
Vidor Gencel <vidorge@gmail.com>
2024-01-11 11:13:43 +0100

    Initial

---
+++ build.js
@@ -0,0 +1,29 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const { SHA3 } = require('sha3');
+const axios = require('axios');
+
+
+;(async () => {
+    const remoteBlocklist = yaml.load((await axios.get('https://raw.githubusercontent.com/phantom/blocklist/master/blocklist.yaml')).data).map((item) => { return item.url });
+    const remoteNftBlocklist = yaml.load((await axios.get('https://raw.githubusercontent.com/phantom/blocklist/master/nft-blocklist.yaml')).data).map((item) => { return item.mint });
+
+    const localBlocklist = yaml.load(fs.readFileSync('./lists/blocklist.yaml', 'utf8')).map((item) => { return item.url });
+    const localNftBlocklist = yaml.load(fs.readFileSync('./lists/nft-blocklist.yaml', 'utf8')).map((item) => { return item.mint });
+
+    const data = {
+        'blocklist': [...new Set([...remoteBlocklist, ...localBlocklist])],
+        'nftBlocklist': [...new Set([...remoteNftBlocklist, ...localNftBlocklist])],
+        'whitelist': [],
+        'fuzzylist': [],
+    }
+
+    const hash = new SHA3(256);
+    hash.update(JSON.stringify(data));
+    const contentHash = hash.digest('hex');
+
+    data['contentHash'] = contentHash;
+
+    fs.writeFileSync('./blocklist.json', JSON.stringify(data));
+    fs.writeFileSync('./content-hash.json', JSON.stringify(contentHash));
+})();
