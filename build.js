c094539ce0a7b523e47fc17c23f8c58b580bc475
Vidor Gencel <vidorge@gmail.com>
2024-01-11 11:32:02 +0100

    support trees for nfts

--- build.js
+++ build.js
@@ -5,8 +5,20 @@ const axios = require('axios');
 
 
 ;(async () => {
-    const remoteBlocklist = yaml.load((await axios.get('https://raw.githubusercontent.com/phantom/blocklist/master/blocklist.yaml')).data).map((item) => { return item.url });
-    const remoteNftBlocklist = yaml.load((await axios.get('https://raw.githubusercontent.com/phantom/blocklist/master/nft-blocklist.yaml')).data).map((item) => { return item.mint });
+    const remoteBlocklist = yaml.load((await axios.get('https://raw.githubusercontent.com/phantom/blocklist/master/blocklist.yaml')).data).map((item) => { 
+        return item.url || null;
+    }).filter(Boolean);
+    const remoteNftBlocklist = yaml.load((await axios.get('https://raw.githubusercontent.com/phantom/blocklist/master/nft-blocklist.yaml')).data).map((item) => {
+        if (item.mint) {
+            return item.mint;
+        }
+
+        if (item.tree) {
+            return item.tree;
+        }
+
+        return null;
+    }).filter(Boolean);
 
     const localBlocklist = yaml.load(fs.readFileSync('./lists/blocklist.yaml', 'utf8')).map((item) => { return item.url });
     const localNftBlocklist = yaml.load(fs.readFileSync('./lists/nft-blocklist.yaml', 'utf8')).map((item) => { return item.mint });
